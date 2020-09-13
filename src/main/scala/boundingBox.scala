76f8a65203965d58f119228b747e168256528a46
nmigacz <nickmigacz@gmail.com>
2020-09-13 17:59:29 -0500

    Ellipse, Rect, Location and Simple Group tests passed

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -4,6 +4,20 @@ package edu.luc.cs.laufer.cs372.shapes
 
 object boundingBox {
   def apply(s: Shape): Location = s match {
+    case Rectangle(width, height) => Location(0, 0, Rectangle(width, height))
+    case Location(x, y, shape) => Location(x, y, shape)
+    case Ellipse(width, height) => Location(-width, -height, Rectangle(2*width, 2*height))
+    case Group(shapes @_*) => {
+      val boundingBoxes = shapes.map(s =>boundingBox(s))
+      val minX = boundingBoxes.map(b => b.x).min
+      val maxX = boundingBoxes.map(b => b.x + b.shape.asInstanceOf[Rectangle].width).max
+      val minY = boundingBoxes.map(b => b.y).min
+      val maxY = boundingBoxes.map(b => b.y + b.shape.asInstanceOf[Rectangle].height).max
+      //val width1 = boundingBoxes.foldLeft(0)((a,b) => a.x + a.shape.asInstanceOf[Rectangle].width.max, b.x + b.shape.asInstanceOf[Rectangle].width.max)
+      val width = maxX - minX
+      val height = maxY-minY
+      Location(minX, minY, Rectangle(width, height))
+    }
     case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
   }
 }
--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -7,8 +7,15 @@ sealed trait Shape
 
 case class Rectangle(width: Int, height: Int) extends Shape
 
+case class Ellipse(width: Int, height: Int) extends Shape
+
 case class Location(x: Int, y: Int, shape: Shape) extends Shape {
   require(shape != null, "null shape in location")
+}
+
+case class Group(shapes:Shape*) extends Shape{
+  require(shapes != null, "null shape in Group")
+  shapes.contains(shapes, "null shape in shapes")
 }
 
 // TODO add missing case classes (see test fixtures)
